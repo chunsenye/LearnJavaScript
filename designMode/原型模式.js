bdafde26ead1e911c79a1ce9aa460aa2f8fae643
chunsenye <30768615+chunsenye@users.noreply.github.com>
2018-10-31 11:45:39 +0800

    1

---
+++ designMode/原型模式.js
@@ -0,0 +1,25 @@
+function Student(name,gender,score){
+    this.name=name;
+    this.gender=gender;
+    this.score=score;
+}
+var a =new Student('l','boy',98);
+var b =new Student('l','boy',98);
+// 当我们使用以上的new 是会创建两个相同的 play方法
+// 我们可以只是使用引用
+
+// 创建一次 多次引用
+
+function play(){
+    console.log('喜欢玩游戏');
+}
+a.play=play;
+console.log(a.play)
+console.log(play)
+a.play()
+
+//原型方式的书写
+
+Student.prototype.ppp=play;
+
+console.log(a.ppp)
---
+++ designMode/构造器模式.js
@@ -0,0 +1,22 @@
+function Student(name,gender,score){
+    this.name=name;
+    this.gender=gender;
+    this.score=score;
+    this.play=function(){
+        console.log('喜欢玩游戏');
+    }
+}
+var a =new Student('l','boy',98);
+console.log(a)
+
+class People{
+    constructor(name,gender){
+        this.name=name;
+        this.gender=gender;
+    }
+    play(){
+        console.log('es6')
+    }
+}
+var b=new People('2','girl')
+console.log(b)
